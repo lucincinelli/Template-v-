a13c2a1d2cd53adf213834cc1ab6fdeb49d3ffa9
Lucas Cincinelli <cincinellilu@gmail.com>
2020-06-11 23:43:02 +0200

    Initial Template V.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php 
+    include_once('includes/root.php')
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=$root?>media/css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
